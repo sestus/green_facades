d6da8c1ec81cff8652ac09d9dbed97a1874ae3d9
Mike Milonakis <michail.milonakis@gmail.com>
2016-04-03 11:42:24 +0300

    Finished Part 2. TODO: control team icon and text under that from mezzanine. Replace blurb icons with porofolio items.

--- theme/admin.py
+++ theme/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from mezzanine.core.admin import TabularDynamicInlineAdmin
+from mezzanine.pages.admin import PageAdmin
+
+from theme.models import (HomePage, Slide, IconBlurb,
+                    Portfolio, PortfolioItem, PortfolioItemCategory,
+                    PortfolioItemImage)
+
+
+class SlideInline(TabularDynamicInlineAdmin):
+    model = Slide
+
+
+class IconInline(TabularDynamicInlineAdmin):
+    model = IconBlurb
+
+
+class HomePageAdmin(PageAdmin):
+    inlines = (SlideInline, IconInline)
+
+
+admin.site.register(HomePage, HomePageAdmin)
+admin.site.register(Portfolio, PageAdmin)
+
+
+class PortfolioItemImageInline(TabularDynamicInlineAdmin):
+    model = PortfolioItemImage
+
+
+class PortfolioItemAdmin(PageAdmin):
+    inlines = (PortfolioItemImageInline,)
+
+admin.site.register(PortfolioItem, PortfolioItemAdmin)
+admin.site.register(PortfolioItemCategory)
